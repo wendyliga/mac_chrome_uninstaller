ead880722815307c9aea46c4f6fd751214c2d4d7
Wendy Liga <wendy.liga@tokopedia.com>
2020-12-14 11:55:36 +0700

    wip script

---
+++ chrome_uninstaller.sh
@@ -0,0 +1,17 @@
+rm -rf /Applications/Google\ Chrome.app/
+rm /Library/LaunchAgents/com.google.keystone*
+rm /Library/LaunchAgents/com.google.keystone*
+rm -rf /Library/Application\ Support/Google/Chrome
+rm ~/Library/Preferences/com.google.Chrome*
+
+rm -rf ~/Applications/Chrome\ Apps.localized/
+rm ~/Library/LaunchAgents/com.google.keystone*
+rm ~/Library/LaunchAgents/com.google.keystone*
+rm -rf ~/Library/Application\ Support/Google/Chrome
+rm ~/Library/Preferences/com.google.Chrome*
+rm -rf ~/Library/Application\ Support/CrashReporter/Google\ Chrome
+rm ~/Library/Preferences/Google\ Chrome*
+rm -r ~/Library/Caches/com.google.Chrome*
+rm -r ~/Library/Saved\ Application\ State/com.google.Chrome.savedState/
+rm ~/Library/Google/GoogleSoftwareUpdate/Actives/com.google.Chrome
+rm ~/Library/Google/Google\ Chrome*
